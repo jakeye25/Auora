e5d0058a1b6655cf83dd36d5b9e162c2f850c6c6
Jake Ye <jayyeucla@gmail.com>
2022-10-26 22:43:36 -0700

    update seed data

--- app/seeds/answers.py
+++ app/seeds/answers.py
@@ -1,12 +1,17 @@
 from app.models import db, Answer, question
+from datetime import datetime, date
+
+now = date.today()
+
+
 
 def seed_answers():
-    answer01 = Answer(answercotent = "The most dangerous book in the world is : Codex Gigas : The Devil's Bible", userId= 2, questionId =1)
-    answer02 = Answer(answercotent = 'Harry Potter and the Order of the Phoenix', userId= 1, questionId =2, answerimage='https://upload.wikimedia.org/wikipedia/en/7/70/Harry_Potter_and_the_Order_of_the_Phoenix.jpg')
-    answer03 = Answer(answercotent = 'The fifth novel, Harry Potter and the Order of the Phoenix, is the longest book in the series', userId= 3, questionId =2)
-    answer04 = Answer(answercotent = '5 hours', userId= 1, questionId =8)
-    answer05 = Answer(answercotent = 'Times are 400 km/80 km/h = 5 hours and 400 km/100 km/h = 4 hours.', userId= 2, questionId =8)
-    answer06 = Answer(answercotent = 'Create an online course.', userId= 1, questionId =6)
+    answer01 = Answer(answercotent = "The most dangerous book in the world is : Codex Gigas : The Devil's Bible", userId= 2, questionId =1, createdAt= now, updatedAt= now)
+    answer02 = Answer(answercotent = 'Harry Potter and the Order of the Phoenix', userId= 1, questionId =2, answerimage='https://upload.wikimedia.org/wikipedia/en/7/70/Harry_Potter_and_the_Order_of_the_Phoenix.jpg', createdAt= now, updatedAt= now)
+    answer03 = Answer(answercotent = 'The fifth novel, Harry Potter and the Order of the Phoenix, is the longest book in the series', userId= 3, questionId =2, createdAt= now, updatedAt= now)
+    answer04 = Answer(answercotent = '5 hours', userId= 1, questionId =8, createdAt= now, updatedAt= now)
+    answer05 = Answer(answercotent = 'Times are 400 km/80 km/h = 5 hours and 400 km/100 km/h = 4 hours.', userId= 2, questionId =8, createdAt= now, updatedAt= now)
+    answer06 = Answer(answercotent = 'Create an online course.', userId= 1, questionId =6, createdAt= now, updatedAt= now)
 
 
     db.session.add(answer01)
--- app/seeds/topics.py
+++ app/seeds/topics.py
@@ -1,11 +1,15 @@
 from app.models import db, Topic
+from datetime import datetime, date
+
+now = date.today()
+
 
 def seed_topics():
-    food = Topic(name='Food', topicimage='https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg')
-    tech = Topic(name='Technology', topicimage='https://www.nato.int/nato_static_fl2014/assets/pictures/images_mfu/2022/7/stock/technology_rdax_775x440s.jpg')
-    travel = Topic(name='Travel', topicimage='https://hips.hearstapps.com/hmg-prod/images/where-to-travel-in-2022-1640200544.jpg')
-    book = Topic(name='Books', topicimage='https://cdn.elearningindustry.com/wp-content/uploads/2016/05/top-10-books-every-college-student-read-1024x640.jpeg')
-    movie = Topic(name='Movies', topicimage='https://s3-us-west-2.amazonaws.com/prd-rteditorial/wp-content/uploads/2018/03/13153742/RT_300EssentialMovies_700X250.jpg')
+    food = Topic(name='Food', topicimage='https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg', createdAt= now, updatedAt= now)
+    tech = Topic(name='Technology', topicimage='https://www.nato.int/nato_static_fl2014/assets/pictures/images_mfu/2022/7/stock/technology_rdax_775x440s.jpg', createdAt= now, updatedAt= now)
+    travel = Topic(name='Travel', topicimage='https://hips.hearstapps.com/hmg-prod/images/where-to-travel-in-2022-1640200544.jpg', createdAt= now, updatedAt= now)
+    book = Topic(name='Books', topicimage='https://cdn.elearningindustry.com/wp-content/uploads/2016/05/top-10-books-every-college-student-read-1024x640.jpeg', createdAt= now, updatedAt= now)
+    movie = Topic(name='Movies', topicimage='https://s3-us-west-2.amazonaws.com/prd-rteditorial/wp-content/uploads/2018/03/13153742/RT_300EssentialMovies_700X250.jpg', createdAt= now, updatedAt= now)
 
 
     db.session.add(food)
