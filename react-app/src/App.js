e49fa93caff8d15d825bd8d366622e244eadc84f
Jake Ye <jayyeucla@gmail.com>
2022-10-27 08:47:22 -0700

    update appjs navbar

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      {/* <NavBar /> */}
+      {loaded && <NavBar />}
       <Switch>
         <Route path='/' exact={true}>
           <LoginForm />
