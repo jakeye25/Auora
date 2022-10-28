6ab4109e5b4bab5f58e307a467efe6d788546df4
Jake Ye <jayyeucla@gmail.com>
2022-10-28 00:19:59 -0700

    update backg image

--- react-app/src/components/Loginpage.js
+++ react-app/src/components/Loginpage.js
@@ -2,16 +2,25 @@ import React from 'react';
 import Demouser from './auth/demouser';
 import LoginForm from './auth/LoginForm';
 import SignupFormModal from './auth/SignUpFormModal';
-
-
+import './Loginpage.css'
 
 const LoginPage = () => {
 
     return (
         <>
-            <SignupFormModal/>
-            <LoginForm/>
-            <Demouser/>
+            <body id='loginbody'>
+                <div id='login_container'>
+                    <div id='loginpage-top'>
+                        <img id='loginpage-logo'
+                        src='https://www.kindpng.com/picc/m/462-4620573_quora-quora-logo-hd-png-download.png'
+                        alt='logo'></img>
+                        <h3>A place to share knowledge and better understand the world</h3>
+                    </div>
+                    <SignupFormModal/>
+                    <LoginForm/>
+                    <Demouser/>
+                </div>
+            </body>
         </>
     )
 
