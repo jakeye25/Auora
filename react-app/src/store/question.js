d32d9b70d6fbbc806906ef0e3d769550b7ebc4de
Jake Ye <jayyeucla@gmail.com>
2022-10-28 10:32:36 -0700

    add question store

---
+++ react-app/src/store/question.js
@@ -0,0 +1,8 @@
+// TYPES
+
+const createQuestion = '/question/createQuestion'
+const getAllQuestion = '/question/getAllQuestion'
+const getCurrentQuestion = '/question/getCurrentQuestion'
+const getOneQuestion = '/question/getOneQuestion'
+const updateQuestion = '/question/updateQuestion'
+const deleteQuestion = '/question/deleteQuestion'
