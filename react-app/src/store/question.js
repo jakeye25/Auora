87d59f7fdf1089c7ea586db57b638a8bcd77eddb
Jake Ye <jayyeucla@gmail.com>
2022-10-28 10:53:03 -0700

    add action creators in question store

--- react-app/src/store/question.js
+++ react-app/src/store/question.js
@@ -6,3 +6,47 @@ const getCurrentQuestion = '/question/getCurrentQuestion'
 const getOneQuestion = '/question/getOneQuestion'
 const updateQuestion = '/question/updateQuestion'
 const deleteQuestion = '/question/deleteQuestion'
+
+// ACTION CREATORS
+
+const actionCreateQustion = (question) => {
+    return {
+        type: createQuestion,
+        question
+    }
+}
+
+const actionGetAllQuestion = (questions) => {
+    return {
+        type: getAllQuestion,
+        questions
+    }
+}
+
+const actionGetCurrentQuestion = (questions) => {
+    return {
+        type: getCurrentQuestion,
+        questions
+    }
+}
+
+const actionGetOneQuestion = (question) => {
+    return {
+        type: getOneQuestion,
+        question
+    }
+}
+
+const actionUpdateQuestion = (question) => {
+    return {
+        type: updateQuestion,
+        question
+    }
+}
+
+const actionDeleteQuestion = (id) => {
+    return {
+        type: deleteQuestion,
+        id
+    }
+}
