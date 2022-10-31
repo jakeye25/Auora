741c804b3842cbf5180b319dda73821cd9cfb4b7
Jake Ye <jayyeucla@gmail.com>
2022-10-30 20:47:06 -0700

    update myanswers component

--- react-app/src/components/Answers/MyAnswerListing.js
+++ react-app/src/components/Answers/MyAnswerListing.js
@@ -15,6 +15,7 @@ function MyAnswerListings() {
     const currentAnswerArr = Object.values(currentAnswer)
 
     const answerfilter = currentAnswerArr.filter(answer => answer?.userId === user?.id);
+    console.log('myanswer', answerfilter)
 
     useEffect(() => {
         // dispatch(thunkGetAllProduct())
@@ -24,8 +25,35 @@ function MyAnswerListings() {
     if (!user) history.push('/')
 
     return (
-        <div>
-            answers
+        <div id="myanswer-container">
+                {answerfilter.length === 0 ? (<h1>You have no answer so far</h1>) : (
+                    <div>
+                        {answerfilter.map((answer, i) => (
+                            <div key={i}>
+                                <div className="my_question_listing_innerbox">
+                                    <div className="my_question_listing_nav">
+                                        <NavLink to={`/questions/${answer?.questionId}`}>
+
+                                        <div className="my_question_listing_name">{answer?.question}</div>
+                                        </NavLink>
+                                    </div>
+                                    <div>{answer?.answercontent}</div>
+                                    {answer.answerimage ? <div><img
+                                        className="qlist-questionimg"
+                                        src={answer?.answerimage}
+                                        alt="img"></img></div> : <div></div>}
+                                    {/* <div id="my_question_listing_btn_container">
+                                        <Link id="userquestioneditbtn" to={`/questions/${question?.id}/edit`}>
+                                            Edit question
+                                        </Link>
+                                        <QuestionDelete question={question} />
+                                    </div> */}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                )}
         </div>
     )
 
