2657b558802c2af20cb879b0af23f590f94e37ad
Jake Ye <jayyeucla@gmail.com>
2022-11-04 15:16:37 -0700

    create search component

---
+++ react-app/src/components/Search/Search.js
@@ -0,0 +1,14 @@
+import {useState, useEffect} from 'react';
+import {NavLink} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+
+
+function Searchbar() {
+    const questions = useSelector((state) => state.question)
+    const answers = useSelector((state) => state.answer)
+
+
+
+}
+
+export default Searchbar
