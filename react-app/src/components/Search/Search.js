1b28476b08729fe2210c53e6951790efaf22e24d
Jake Ye <jayyeucla@gmail.com>
2022-11-06 11:29:04 -0800

    update search component

--- react-app/src/components/Search/Search.js
+++ react-app/src/components/Search/Search.js
@@ -1,17 +1,53 @@
 import {useState, useEffect} from 'react';
 import {NavLink} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
+import { thunkGetAllQuestion } from '../../store/question';
 
 
 function Searchbar() {
+    const dispatch = useDispatch();
     const questions = useSelector((state) => state.question)
     const answers = useSelector((state) => state.answer)
 
     const questionsArr = Object.values(questions)
-    const answers
+    const answersArr = Object.values(answers)
+
+    const [searchWord, setSearchWord] = useState('')
+    const [showDropdown, setShowDropdown] = useState(false);
+    const[searchResult, setSearchResult] =useState([]);
+
+    const results = (word) =>{
+        const str =[];
+        for (let i =0; i<questions.length; i++){
+          let question = questions[i];
+          if (question.questioncontent.toLowerCase().includes(word.toLowerCase())
+          ){
+            str.push(question)
+          }
+        }
+        return str
+    }
+
+    useEffect(() => {
+        if (searchWord.length) {
+            setShowDropdown(true)
+            setSearchResult(results(searchWord))
+        } else {
+            setShowDropdown(false)
+            setSearchResult([])
+        }
+    }, [searchWord])
+
+    useEffect(() => {
+        dispatch(thunkGetAllQuestion())
+    }, [dispatch])
+
+    useEffect(()=>{
+
+    }, [questions])
 
     return(
-        <div>
+        <div className='searchbar'>
 
         </div>
     )
