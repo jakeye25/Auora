75d8cc24930389f29e3be614a686357be251c57e
Jake Ye <jayyeucla@gmail.com>
2023-01-29 22:00:16 -0800

    add sociall links

--- react-app/src/components/Social_links/social.js
+++ react-app/src/components/Social_links/social.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 
-const SocialLinks =() => {
+const SocialLinks = () => {
 
 
-    return(
-        <div>
-
-        </div>
+    return (
+        <nav class="social">
+            <ul>
+                <li><a href="https://jakeye25.github.io/">Portfolio <i className="fa fa-twitter"></i></a></li>
+                <li><a href="https://github.com/jakeye25">Github <i className="fa fa-github"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/jake-ye-a2365250/">Linkedin <i className="fa fa-linkedin"></i></a></li>
+                <li><a href="mailto:jayyeucla@gmail.com">Email <i className="fa fa-linkedin"></i></a></li>
+            </ul>
+        </nav>
     );
 }
 
